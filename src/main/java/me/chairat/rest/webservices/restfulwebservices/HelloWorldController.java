6d144ea5a60e388cca82ab9596031abf4ac9b10b
Par <pacroy@gmail.com>
2019-03-17 15:04:14 +0800

    Removed /hello-world2

--- src/main/java/me/chairat/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/me/chairat/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,14 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World!";
-    }
-
-    @GetMapping("/hello-world2")
-    public String helloWorld2() {
-        return "Hello World 2!";
     }
 
 }
