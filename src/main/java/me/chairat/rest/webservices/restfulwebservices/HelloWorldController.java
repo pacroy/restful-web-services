0589b165c3a0058393583fec17f1dc24a02e3304
Par <pacroy@gmail.com>
2019-03-17 15:01:32 +0800

    Added 2nd endpoint /hello-world2

--- src/main/java/me/chairat/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/me/chairat/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package me.chairat.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,11 @@ public class HelloWorldController {
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @GetMapping("/hello-world2")
+    public String helloWorld2() {
+        return "Hello World 2!";
     }
 
 }
