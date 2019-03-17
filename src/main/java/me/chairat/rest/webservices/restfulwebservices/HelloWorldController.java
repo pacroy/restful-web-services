b539e3a511f62f38ca670fd8f3797bb70a629d39
Par <pacroy@gmail.com>
2019-03-17 14:57:46 +0800

    ADded /hello-world endpoint

---
+++ src/main/java/me/chairat/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,15 @@
+package me.chairat.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
